fe314d8ead9c57369c1f56935b42398011942f72
Silvan Dähn <silvandaehn@me.com>
2015-02-22 22:04:25 +0000

    Initial

---
+++ server.js
@@ -0,0 +1,35 @@
+var exec = require('child_process').exec;
+var bodyParser = require("body-parser");
+var express = require('express')
+var sys = require('sys')
+
+var app = express()
+app.use(bodyParser.json());
+
+function puts(error, stdout, stderr) { 
+	console.log(stdout) 
+}
+
+function changeSwitch(id, unit, state) {
+	var stateString = state ? "1" : "0"
+	var params = id + " " + unit + " " + stateString
+	exec("cd ../rcswitch-pi/ && sudo ./send " + params, puts)
+}
+
+app.get('/', function (req, res) {
+    res.send('Server up and running!')
+})
+
+app.post('/switch', function (req, res) {
+	var id = req.body.id
+	var unit = req.body.unit
+	var state = req.body.state
+	changeSwitch(id, unit, state)
+	res.send(req.body)
+})
+
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log('Server listening at http://%s:%s', host, port)
+})
